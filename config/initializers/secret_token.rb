af859047251bb85c2400b6c718be0af162baef5c
France-Nor Brute <fbrute@gmail.com>
2013-11-03 12:30:31 -0400

    adding OpenShift files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virmuse::Application.config.secret_token = '893c48f473cdfc78a892777e9434a709227594ceea1b9a56b660c3e61503d3cbdc2aa86b7fd65752254246e3ebb0cf741d3279e466a764a18a4731f364d2973e'
